098b1fff9d46d6a9baa4cfa78920e30aaa51548c
luriel-xyz <luriel.mapili@yahoo.com>
2020-10-02 15:34:56 +0800

    Add updates

--- js/main.js
+++ js/main.js
@@ -5,5 +5,45 @@ AOS.init({
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".year").innerHTML = new Date().getFullYear();
+  const setYear = () => {
+    const currentYear = new Date().getFullYear();
+    document.querySelector(".year").innerHTML = currentYear;
+  };
+  setYear();
+
+  const getRandomColor = () => {
+    const colors = ["#fff", "#fd4d40", "#ffc107"];
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    return randomColor;
+  };
+
+  const animateIntro = () => {
+    new TypeIt(".intro", {
+      strings: "I'm Keith!",
+      loop: true,
+      afterStep: function (step, instance) {
+        instance.getElement().style.color = getRandomColor();
+      },
+    }).go();
+  };
+
+  animateIntro();
 });
+
+window.onload = function () {
+  Particles.init({
+    selector: ".background",
+    color: "#fff",
+    sizeVariations: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          color: ["#fff", "#fd4d40", "#ffc107"],
+          sizeVariations: 4,
+        },
+      },
+    ],
+  });
+};
